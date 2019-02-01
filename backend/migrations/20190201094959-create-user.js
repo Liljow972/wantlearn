08f7d16806a1686c81d1d5614133557b01941afd
zineb <zineb.azzouzi@hotmail.fr>
2019-02-01 11:33:17 +0100

    login & register

---
+++ backend/migrations/20190201094959-create-user.js
@@ -0,0 +1,56 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      firstname: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      lastname: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      birthday: {
+        allowNull: true,
+        type: Sequelize.DATE
+      },
+      bio: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      image: {
+        allowNull: true,
+        type: Sequelize.BLOB
+      },
+      email: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      password: {
+        allowNull: false,
+        type: Sequelize.STRING
+      },
+      location: {
+        allowNull: true,
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
